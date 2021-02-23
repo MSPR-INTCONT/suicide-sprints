6f94524579404b857acb936d91e075bf5c1a77e2
stephannicolas96 <stephannicolas96@hotmail.com>
2021-02-23 15:12:26 +0100

    update roll method

--- Trivia/Trivia/Game.cs
+++ Trivia/Trivia/Game.cs
@@ -68,40 +68,40 @@ namespace Trivia
             if (_inPenaltyBox[_currentPlayer])
             {
                 RollWhenInPenaltyBox(roll);
+
+                if (_isGettingOutOfPenaltyBox)
+                    NewQuestion();
             }
             else
             {
                 RollWhenNotInPenaltyBox(roll);
 
-                Console.WriteLine(_players[_currentPlayer]
-                                  + "'s new location is "
-                                  + _places[_currentPlayer]);
-                Console.WriteLine("The category is " + CurrentCategory());
-                AskQuestion();
+                NewQuestion();
             }
+        }
+
+        private void NewQuestion()
+        {
+            Console.WriteLine(_players[_currentPlayer]
+                              + "'s new location is "
+                              + _places[_currentPlayer]);
+            Console.WriteLine("The category is " + CurrentCategory());
+            AskQuestion();
         }
 
         private void RollWhenInPenaltyBox(int roll)
         {
-            if (roll % 2 != 0)
-            {
-                _isGettingOutOfPenaltyBox = true;
-
-                Console.WriteLine(_players[_currentPlayer] + " is getting out of the penalty box");
-                _places[_currentPlayer] = _places[_currentPlayer] + roll;
-                if (_places[_currentPlayer] > 11) _places[_currentPlayer] = _places[_currentPlayer] - 12;
-
-                Console.WriteLine(_players[_currentPlayer]
-                                  + "'s new location is "
-                                  + _places[_currentPlayer]);
-                Console.WriteLine("The category is " + CurrentCategory());
-                AskQuestion();
-            }
-            else
+            if (roll % 2 == 0)
             {
                 Console.WriteLine(_players[_currentPlayer] + " is not getting out of the penalty box");
                 _isGettingOutOfPenaltyBox = false;
             }
+
+            _isGettingOutOfPenaltyBox = true;
+
+            Console.WriteLine(_players[_currentPlayer] + " is getting out of the penalty box");
+            _places[_currentPlayer] = _places[_currentPlayer] + roll;
+            if (_places[_currentPlayer] > 11) _places[_currentPlayer] = _places[_currentPlayer] - 12;
         }
 
         private void RollWhenNotInPenaltyBox(int roll)
