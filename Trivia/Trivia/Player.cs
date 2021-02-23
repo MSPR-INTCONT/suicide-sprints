5ffd2b5d4b40feab596226883de108b7dc0c9d12
AlexLecq <alex.lecq6@gmail.com>
2021-02-23 17:07:21 +0100

    add PLayer class

---
+++ Trivia/Trivia/Player.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+namespace Trivia
+{
+    public class Player
+    {
+        private readonly int _places;
+        private readonly int _purses;
+        private readonly bool _inPenaltyBox;
+
+        public Player()
+        {
+            
+        }
+    }
+}
