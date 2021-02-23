c4cfbd8df4655df8befb32df403ff904013a2751
stephannicolas96 <stephannicolas96@hotmail.com>
2021-02-23 15:30:17 +0100

    refacto roll

--- Trivia/Trivia/Game.cs
+++ Trivia/Trivia/Game.cs
@@ -91,17 +91,15 @@ namespace Trivia
 
         private void RollWhenInPenaltyBox(int roll)
         {
-            if (roll % 2 == 0)
+            _isGettingOutOfPenaltyBox = roll % 2 != 0;
+            if(!_isGettingOutOfPenaltyBox)
             {
                 Console.WriteLine(_players[_currentPlayer] + " is not getting out of the penalty box");
-                _isGettingOutOfPenaltyBox = false;
+                return;
             }
 
-            _isGettingOutOfPenaltyBox = true;
-
             Console.WriteLine(_players[_currentPlayer] + " is getting out of the penalty box");
-            _places[_currentPlayer] = _places[_currentPlayer] + roll;
-            if (_places[_currentPlayer] > 11) _places[_currentPlayer] = _places[_currentPlayer] - 12;
+            RollWhenNotInPenaltyBox(roll);
         }
 
         private void RollWhenNotInPenaltyBox(int roll)
